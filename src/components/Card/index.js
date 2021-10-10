2ff204f36b0de5144ec5fb96168584f53c16c55d
Mrezagolbaba <m.reza.golbaba@gmail.com>
2021-10-10 22:31:29 +0330

    update card item

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,7 +2,29 @@ import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 
 function CardItem(props) {
-  const {name, symbol, all_time_high} = props.data;
+  const {
+    name,
+    symbol,
+    all_time_high: {price, percent_down},
+    market_data: {percent_change_usd_last_24_hours: change},
+  } = props.data;
+
+  const convertPercent = data => {
+    if (data > 0) {
+      return (
+        <Text style={styles.positiveNumber}>
+          {`%` + `${Math.round(data * 100) / 100}`}
+        </Text>
+      );
+    } else {
+      return (
+        <Text style={styles.negativeNumber}>
+          {`%` + `${Math.round(data * 100) / 100}`}
+        </Text>
+      );
+    }
+  };
+
   return (
     <View style={styles.cardContainer}>
       <View>
@@ -10,8 +32,9 @@ function CardItem(props) {
         <Text>{name}</Text>
         <Text>{symbol}</Text>
       </View>
-      <View>
-        <Text>{`$` + `${Math.round(all_time_high.price * 100) / 100}`}</Text>
+      <View style={styles.holderPrice}>
+        <Text>{`$` + `${Math.round(price * 100) / 100}`}</Text>
+        {convertPercent(change)}
       </View>
     </View>
   );
@@ -25,6 +48,20 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingVertical: 40,
     paddingHorizontal: 30,
+    borderBottomWidth: 0.5,
+    borderColor: 'gray',
+    width: '95%',
+  },
+  holderPrice: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  positiveNumber: {
+    color: 'green',
+  },
+  negativeNumber: {
+    color: 'red',
   },
 });
 export default CardItem;

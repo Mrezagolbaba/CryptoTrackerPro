c6e1dcaf72ffa3ca2b049cd5006b2d80d1ce5786
Mrezagolbaba <m.reza.golbaba@gmail.com>
2021-10-10 22:31:15 +0330

    add wrapper for app

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
+import {Provider} from 'react-redux';
+import configureStore from './src/utils/store';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const AppWrapper = () => {
+  const store = configureStore();
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => AppWrapper);

686623f0f939e66aaa3864ca7a4f26bde7dd96f3
Mrezagolbaba <m.reza.golbaba@gmail.com>
2021-10-11 18:39:41 +0330

    add swip mode to card view

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-
+import {StyleSheet, View, Text, Animated} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import {RectButton} from 'react-native-gesture-handler';
 function CardItem(props) {
   const {
+    id,
     name,
     symbol,
     all_time_high: {price, percent_down},
@@ -12,31 +15,67 @@ function CardItem(props) {
   const convertPercent = data => {
     if (data > 0) {
       return (
-        <Text style={styles.positiveNumber}>
-          {`%` + `${Math.round(data * 100) / 100}`}
-        </Text>
+        <View style={styles.containerPersent}>
+          <Icon name="arrow-top-right" size={20} color="green" />
+          <Text style={styles.positiveNumber}>
+            {`%` + `${Math.round(data * 100) / 100}`}
+          </Text>
+        </View>
       );
     } else {
       return (
-        <Text style={styles.negativeNumber}>
-          {`%` + `${Math.round(data * 100) / 100}`}
-        </Text>
+        <View style={styles.containerPersent}>
+          <Icon name="arrow-bottom-left" size={20} color="red" />
+          <Text style={styles.negativeNumber}>
+            {`%` + `${Math.round(data * 100) / 100}`}
+          </Text>
+        </View>
       );
     }
   };
 
+  const renderLeftActions = (progress, dragX) => {
+    const trans = dragX.interpolate({
+      inputRange: [-100, 0],
+      outputRange: [0.7, 0],
+    });
+    return (
+      <RectButton
+        style={styles.leftAction}
+        onPress={() => props.handleDelete(id)}>
+        <Animated.Text
+          style={[
+            styles.actionText,
+            {
+              transform: [{translateX: trans}],
+            },
+          ]}>
+          Remove
+        </Animated.Text>
+      </RectButton>
+    );
+  };
+
   return (
-    <View style={styles.cardContainer}>
-      <View>
-        {/* <Image /> */}
-        <Text>{name}</Text>
-        <Text>{symbol}</Text>
+    <Swipeable
+      containerStyle={{width: '100%'}}
+      renderLeftActions={renderLeftActions}>
+      <View style={styles.cardContainer}>
+        <View>
+          {/* <Image /> */}
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.abbreviation}>{symbol}</Text>
+        </View>
+        <View style={styles.holderPrice}>
+          {price && (
+            <Text style={styles.price}>
+              {`$` + `${Math.round(price * 100) / 100}`}
+            </Text>
+          )}
+          {convertPercent(change)}
+        </View>
       </View>
-      <View style={styles.holderPrice}>
-        <Text>{`$` + `${Math.round(price * 100) / 100}`}</Text>
-        {convertPercent(change)}
-      </View>
-    </View>
+    </Swipeable>
   );
 }
 
@@ -50,18 +89,48 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
     borderBottomWidth: 0.5,
     borderColor: 'gray',
-    width: '95%',
+    width: '100%',
   },
   holderPrice: {
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
+    alignItems: 'flex-end',
   },
   positiveNumber: {
     color: 'green',
   },
   negativeNumber: {
     color: 'red',
+  },
+  name: {
+    fontSize: 16,
+    color: 'black',
+    fontWeight: '500',
+  },
+  abbreviation: {
+    color: 'black',
+    fontWeight: '300',
+  },
+  price: {
+    color: 'black',
+    fontWeight: '500',
+    fontSize: 16,
+  },
+  containerPersent: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  leftAction: {
+    display: 'flex',
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  actionText: {
+    color: 'white',
+    fontWeight: '500',
+    paddingHorizontal: 10,
   },
 });
 export default CardItem;
